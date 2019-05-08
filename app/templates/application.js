e3f7392b7d3e4b7d996d350ad784d233118712a0
Chris Manson <chris@manson.ie>
2019-05-08 14:37:07 +0100

    initial working commit

---
+++ app/templates/application.js
@@ -0,0 +1,1 @@
+export { default } from 'rfc-process-mdbook-template/templates/application';
--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 'use strict';
 
+var Funnel = require('broccoli-funnel');
+
+const { join } = require('path');
+
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+
+  treeForPublic: function() {
+    return new Funnel(join(this.root, 'public'));
+  },
+
+  contentFor: function(type) {
+    if (type === 'head') {
+      // preload the most common fonts for modern browsers
+      return `<link rel="preload" as="font" type="font/woff2" href="/fonts/open-sans-v16-latin-regular.woff2" crossorigin>
+<link rel="preload" as="font" type="font/woff2" href="/fonts/open-sans-v16-latin-700.woff2" crossorigin>`;
+    }
+  }
 };

4fcddec222dcb8603542edf8b70acbf97140c919
Chris Manson <chris@manson.ie>
2022-04-28 13:40:43 +0100

    add format-date helper

---
+++ addon/helpers/format-date.js
@@ -0,0 +1,11 @@
+import { helper } from '@ember/component/helper';
+
+export default helper(function formatDate(params) {
+  let [date] = params;
+
+  if (!date) {
+    return;
+  }
+
+  return new Intl.DateTimeFormat().format(date, { dateStyle: 'full'});
+});
